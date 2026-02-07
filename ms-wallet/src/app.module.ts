c9b76055a020884bf11addd07c9e39b5fd9d3773
Matheus Prachedes Batista <mortzion@hotmail.com>
2026-02-07 14:01:46 -0300

    feat: add ms-wallet boilerplate with fastify and env loading

---
+++ ms-wallet/src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { HealthCheckController } from './health-check.controller';
+
+@Module({
+  imports: [ConfigModule.forRoot()],
+  controllers: [HealthCheckController],
+  providers: [],
+})
+export class AppModule {}
---
+++ ms-wallet/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  if (!process.env.PORT) throw new Error('PORT env missing');
+
+  await app.listen(process.env.PORT);
+}
+bootstrap();
