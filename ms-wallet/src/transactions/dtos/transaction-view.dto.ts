efddd6525e9de3332a6f3282c5c8e2e4cf136b14
Matheus Prachedes Batista <mortzion@hotmail.com>
2026-02-07 14:01:48 -0300

    fix: fix user_id type in ms-wallet to match the user model in ms-users

--- ms-wallet/src/transactions/dtos/transaction-view.dto.ts
+++ ms-wallet/src/transactions/dtos/transaction-view.dto.ts
@@ -4,7 +4,7 @@ export class TransactionViewDto {
   constructor(
     private id: string,
     private amount: number,
-    private user_id: number,
+    private user_id: string,
     private type: TransactionType,
   ) {}
 }
